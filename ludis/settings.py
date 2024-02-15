a903ac2a632dd9b9a76d3d2ed8127249de96fd8e
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-02-15 14:30:23 +0300

    Switch default database engine from `django.db.backends.postgresql` to `django.db.backends.postgresql_psycopg2`.

--- ludis/settings.py
+++ ludis/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'ludis.wsgi.application'
 
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.postgresql',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'bilihcare',
     'USER': 'Ludis-ET',
     'PASSWORD': 'XSn74iQcolKq',
